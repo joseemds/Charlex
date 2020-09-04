6c2f41639a132a002a7f759ede81713d6f9863cd
José Eduardo <josee.edumont@gmail.com>
2020-09-04 15:29:38 -0300

    Created implementation for commands

---
+++ lib/charlex/command.ex
@@ -0,0 +1,24 @@
+defmodule Charlex.Command do
+  @callback usage() :: [String.t()]
+
+  @callback description() :: String.t()
+
+  @callback parse_args(args :: String.t()) :: any()
+
+  @callback run(context :: Map.t(), args :: [String.t()] | any()) :: String.t() | any()
+
+  defmacro __using__(_opts) do
+    quote do
+      @behavior unquote(__MODULE__)
+
+      @impl true
+      def usage, do: []
+
+      @impl true
+      def description, do: []
+
+      @impl true
+      def run(_context, _args), do: :ok
+    end
+  end
+end
