e089b2757d73eb00c5a9822023a351def7930da3
José Eduardo <josee.edumont@gmail.com>
2020-09-04 17:40:27 -0300

    Fixed bug of bot answering himself

--- lib/charlex/consumer.ex
+++ lib/charlex/consumer.ex
@@ -1,19 +1,19 @@
 defmodule Charlex.Consumer do
   use Nostrum.Consumer
   require Logger
-  alias Nostrum.Api
 
   def start_link do
     Consumer.start_link(__MODULE__)
   end
 
-  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
+  def handle_event({:MESSAGE_CREATE, %{author: %{bot: bot}} = msg, _ws_state}) when bot != true do
+    Charlex.Handler.handle_message(msg)
   end
 
   def handle_event({:READY, _msg, _ws_state}) do
     alias Charlex.Command
 
-    %{}
+    %{"plan" => Command.Plan}
     |> Charlex.put_commands()
 
     Charlex.fetch_owner()
