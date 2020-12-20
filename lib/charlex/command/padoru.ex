4a35714c4522e3327c648cb6c9cbea6626272fcd
José Eduardo <josee.edumont@gmail.com>
2020-12-20 18:50:26 -0300

    Now padoru works flawless every year(i hope so)

--- lib/charlex/command/padoru.ex
+++ lib/charlex/command/padoru.ex
@@ -2,11 +2,27 @@ defmodule Charlex.Command.Padoru do
   use Charlex.Command
   alias Nostrum.Api
 
+  def get_left_days(date) do
+    case Timex.diff(date, Timex.now(), :days) do
+      1 ->
+        "OMG ALMOST THERE ONLY ONE DAY"
+
+      0 ->
+        "HAPPY PADORUUUUUUUUUUUUUUUUUUUUU"
+
+
+      _ ->
+        "#{Timex.diff(date, Timex.now("America/Sao_Paulo"), :days)} days until christmas"
+    end
+  end
+
   @impl true
   def run(context, _args) do
+    christmas_date = Timex.from_iso_day(360) 
+
     Api.create_message(
       context.message.channel_id,
-      "#{Timex.diff(~D[2020-12-25], Timex.now("America/Sao_Paulo"), :days)} days until Christmas"
+      get_left_days(christmas_date)
     )
   end
 end
